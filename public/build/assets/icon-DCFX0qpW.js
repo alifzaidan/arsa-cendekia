1743315f218a001a6dc2296000636e02fb808f3e
alifzaidan <zaidanalif22710@gmail.com>
2025-10-08 05:44:57 +0700

    update build

--- public/build/assets/icon-DCFX0qpW.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-0I1QzVTg.js";import{c as t}from"./utils-liWpcwDK.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
