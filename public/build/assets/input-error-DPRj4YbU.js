05f2bd5b73de23c8483998c12a4d0275a2cadb24
alifzaidan <zaidanalif22710@gmail.com>
2025-10-17 20:59:45 +0700

    update build

--- public/build/assets/input-error-DPRj4YbU.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DJanRBGl.js";import{c as o}from"./utils-DR74k2Cn.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
